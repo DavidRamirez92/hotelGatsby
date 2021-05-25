e1025b50d3f726289dc3c865979a285a59f5e72f
David <davidramirez9210@gmail.com>
2021-05-25 00:10:58 -0300

    finish project

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,19 +2,31 @@ import React from 'react';
 import Header from './header';
 import {Global, css} from '@emotion/react';
 import Helmet from 'react-helmet';
+import Footer from './footer';
+import useSeo from '../hooks/use-seo';
 
 const Layout = (props) => {
+
+   const seo = useSeo();
+  const {siteName, fallbackSeo: {description, title} } = seo;
     return (
         <>
         <Global
             styles={css`
             html{
                 font-size: 62.5%;
+                box-sizing:border-box;
             }
-                body{
-                   font-size: 16px;
-                   font-size:1.6rem;
+
+            *, *:before, *:after {
+                box-sizing: inherit;
+            }
+
+            body{
+                   font-size: 18px;
+                   font-size:1.8rem;
                    line-height: 1.5;
+                   font-family: 'PT Sans',sans-serif;
                 }
                 h1,h2,h3 {
                     margin: 0;
@@ -34,12 +46,16 @@ const Layout = (props) => {
             `}
         />
         <Helmet>
-            <title>Gatsby Hotel</title>
+            <title>{title}</title>
+            <meta name="description" content={description} />
             <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" rel="stylesheet"/>
             <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
         </Helmet>
         <Header/>
-         {props.children}
+            {props.children}
+         <Footer
+            title={title}
+         />
         </>
      );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,55 @@
 import * as React from "react";
+import {css} from '@emotion/react';
+import styled from '@emotion/styled';
 import Layout from '../components/layout';
+import ImagenHotel from '../components/imagenHotel';
+import ContenidoInicio from '../components/contenidoInicio';
+import HabitacionPreview from '../components/habitacionPreview';
+import useHabitaciones from '../hooks/use-habitaciones';
 
-const IndexPage = () => (
-  <Layout>
-  <h1>Index</h1>
-  </Layout>
-)
+const ListadoHabitaciones = styled.ul`
+  max-width: 1200px;
+  width: 95%;
+  margin: 4rem auto 0 auto;
+
+  @media(min-width:768px){
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 3rem;
+  }
+`;
+
+const IndexPage = () => {
+
+const habitaciones = useHabitaciones();
+
+  return(
+    <Layout>
+      <ImagenHotel/>
+      <ContenidoInicio/>
+  
+    <h2
+      css={css`
+        text-align: center;
+        margin-top: 5rem;
+        font-size: 3rem;
+      `}
+    >
+      Nuestra Habitaciones
+    </h2>
+
+    <ListadoHabitaciones>
+      {habitaciones.map(habitacion => (
+        <HabitacionPreview
+          key={habitacion.id}
+          habitacion = {habitacion}
+        />
+
+      ))}
+    </ListadoHabitaciones>
+  
+    </Layout>
+  )
+}
 
 export default IndexPage
